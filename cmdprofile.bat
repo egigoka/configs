b2413dd4cf5864a454364bfcbd51acf15c9e49b6
Egor Egorov <egigoka@gmail.com>
2020-07-02 17:18:17 +0500

    configs for windows terminal and wsl zsh

---
+++ cmdprofile.bat
@@ -0,0 +1,9 @@
+@echo off 
+doskey k=kubectl $*
+doskey d=docker $*
+doskey gp=git pull
+doskey gc=git commit -a -m "$*"
+
+doskey killall=taskkill /f /im $*
+doskey py=python $*
+doskey open=start "" "$*"
