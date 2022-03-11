e5525d672c16c2497f52948052ad303d1e542d58
Egor Egorov <egigoka@gmail.com>
2022-03-12 02:52:07 +0500

    change p10k context colors a bit

--- .p10k.zsh
+++ .p10k.zsh
@@ -870,7 +870,7 @@
 
   ##################################[ context: user@hostname ]##################################
   # Context color when running with privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=160
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=124
   #typeset -g POWERLEVEL9K_CONTEXT_ROOT_BACKGROUND=203
   # Context color in SSH without privileges.
   typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=039
