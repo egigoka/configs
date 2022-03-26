cb5c4003973fda397173af796a0a1c516b7bdd10
Egor Egorov <egigoka@gmail.com>
2022-03-27 01:30:14 +0500

    added iconsur.sh

---
+++ iconsur.sh
@@ -0,0 +1,45 @@
+# See https://github.com/rikumi/iconsur
+
+sudo iconsur set -l /Applications/EaseUS\ MobiMover.app -s 1
+sudo iconsur set /Applications/Utilities/Adobe\ Creative\ Cloud\ Experience/CCXProcess.app -l -s 1.16
+sudo iconsur set -l /Applications/Utilities/Adobe\ Sync/CoreSync/Core\ Sync.app -s 1.16
+sudo iconsur set -l /Applications/Exporter.app -s 1.013
+sudo iconsur set /Applications/Right\ Click.app -l -s 1
+sudo iconsur set /Applications/Smart\ Scroll.app -l -s 1.06
+sudo iconsur set /Applications/Outline.app -l -s 1.1
+
+sudo iconsur set /Applications/Plex\ Media\ Player.app
+sudo iconsur set /Applications/PureVPN.app
+sudo iconsur set /Applications/timetrack.io.app -k "Time Tracker - Tracking Hours"
+
+sudo iconsur set /Applications/Adobe\ Zii\ 2021\ 6.1.7.app -l
+sudo iconsur set /Applications/Adobe\ Zii\ 2022\ 7.0.0.app -l
+sudo iconsur set /Applications/XCode\ Additional\ Tools/Audio/AU\ Lab.app -l
+sudo iconsur set /Applications/Backgroundifier.app -l
+sudo iconsur set /Applications/Clicker\ for\ YouTube\ Launcher.app -l
+sudo iconsur set /Applications/Clicker\ for\ YouTube.app -l
+sudo iconsur set -l /Applications/DuckDuckGo\ Privacy\ Essentials.app
+sudo iconsur set -l /Applications/Kap.app
+sudo iconsur set -l /Applications/NightTone.app
+sudo iconsur set -l /Applications/XCode\ Additional\ Tools/Graphics/OpenGL\ Driver\ Monitor.app
+sudo iconsur set -l /Applications/XCode\ Additional\ Tools/Graphics/OpenGL\ Profiler.app
+sudo iconsur set -l /Applications/XCode\ Additional\ Tools/Graphics/Pixie.app
+sudo iconsur set -l /Applications/XCode\ Additional\ Tools/Graphics/Quartz\ Debug.app
+sudo iconsur set -l /Applications/XCode\ Additional\ Tools/Hardware/Printer\ Simulator.app
+sudo iconsur set -l /Applications/XCode\ Additional\ Tools/Hardware/HomeKit\ Accessory\ Simulator.app
+sudo iconsur set /Applications/SensibleSideButtons.app -l
+sudo iconsur set /Applications/Servo.app -l
+sudo iconsur set /Applications/TaskExplorer.app -l
+sudo iconsur set /Applications/WireGuard.app -l
+sudo iconsur set /Applications/iBetterCharge.app -l
+
+sudo iconsur set /Applications/BreakTimer.app -l; echo fail\?
+sudo iconsur set /Applications/CheckNonceGUI.app -l; echo fail\?
+sudo iconsur set -l /Applications/XCode\ Additional\ Tools/Utilities/CrashReporterPrefs.app; echo fail\?
+sudo iconsur set /Applications/CustomShortcuts.app -l; echo fail\?
+sudo iconsur set -l /Applications/XCode\ Additional\ Tools/Utilities/Help\ Indexer.app; echo fail\?
+ications/XCode\ Additional\ Tools/Utilities/Clipboard\ Viewer.app -l; echo fail\?
+
+sudo iconsur cache
+
+echo also you can set those icons premanently with Replacicon https://replacicon.app
