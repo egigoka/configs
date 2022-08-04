58de5ed2503a4c22edc118f8a67d21739a341982
Egor Egorov <egigoka@gmail.com>
2022-08-04 13:41:28 +0300

    disabled transient prompt

--- .p10k.zsh
+++ .p10k.zsh
@@ -1652,7 +1652,7 @@ function prompt_my_screen_status()
   #   - always:   Trim down prompt when accepting a command line.
   #   - same-dir: Trim down prompt when accepting a command line unless this is the first command
   #               typed after changing current working directory.
-  typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=same-dir
+  typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=off
 
   # Instant prompt mode.
   #
