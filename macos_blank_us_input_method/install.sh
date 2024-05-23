f2ff8c4e1c1f761351c7a648beae31a76e4a3fd4
Egor Egorov <egigoka@gmail.com>
2024-05-23 23:11:35 +0500

    zellij fix alt keys macos

---
+++ macos_blank_us_input_method/install.sh
@@ -0,0 +1,12 @@
+echo don\'t use this, refer to file iterm_fix_alt_key.txt
+
+
+#cp USKeylayout_custom_no_alt.keylayout ~/Library/Keyboard\ Layouts
+#echo now go to Settings - Keyboard - Text Input - click Edit... 
+#echo - click + sign - scroll and click to Others - select Unicode Hex Input
+#echo - click Add - change input method - test - now you may remove old En
+#echo input method - enjoy
+#echo 
+#echo you may need to relogin to this new input method to appear
+
+# https://stackoverflow.com/a/24100077/6519078
