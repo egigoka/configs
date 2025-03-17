2668d8c32f32702a76a0a09398947ef254582149
root <root@proxmox.local>
2025-03-17 08:26:24 +0500

    fixed root color in powerlevel10k

--- .p10k.zsh
+++ .p10k.zsh
@@ -1033,7 +1033,7 @@ function prompt_my_screen_status()
 
   ##################################[ context: user@hostname ]##################################
   # Context color when running with privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=124
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=009
   #typeset -g POWERLEVEL9K_CONTEXT_ROOT_BACKGROUND=203
   # Context color in SSH without privileges.
   typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=039
