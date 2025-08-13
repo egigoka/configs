bac8bf2573b9c57d8b688c259be260b6bf24f83a
egigoka <egigoka@gmail.com>
2025-08-13 15:16:37 +0500

    removed dependency for zellij_attach function in z function

--- fish/functions/z.fish
+++ fish/functions/z.fish
@@ -6,7 +6,7 @@ function z
 
     set session_name $argv
 
-    zellij --session $session_name; or zellij_attach $session_name
+    zellij --session $session_name; or zellij attach $session_name
 
     set exited_sessions (zellij list-sessions --no-formatting 2>/dev/null| \
         string match -r "^$session_name\s+EXITED")
