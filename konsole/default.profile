1aea10489fc999d39fa2e5fc9ca3b665b043fd0e
egigoka <egigoka@gmail.com>
2026-02-12 19:13:48 +0500

    Add GNOME Terminal Light theme for Konsole

---
+++ konsole/default.profile
@@ -0,0 +1,17 @@
+[Appearance]
+ColorScheme=GNOMETerminalLight
+Font=MesloLGSDZ Nerd Font Mono,12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1
+UseFontBrailleChararacters=false
+
+[General]
+Command=/run/current-system/sw/bin/fish
+Name=default
+Parent=FALLBACK/
+TerminalCenter=false
+TerminalMargin=1
+
+[Scrolling]
+HistorySize=10000
+
+[Terminal Features]
+FlowControlEnabled=false
--- setup.sh
+++ setup.sh
@@ -208,6 +208,8 @@ install_link ~/configs/mpv ~/.config/mpv
 install_link "$HOME/configs/konsole/sessionui.rc" "$HOME/.local/share/kxmlgui5/konsole/sessionui.rc"
 install_link "$HOME/configs/konsole/konsoleui.rc" "$HOME/.local/share/kxmlgui5/konsole/konsoleui.rc"
 install_link "$HOME/configs/konsole/konsolerc" "$HOME/.config/konsolerc"
+install_link "$HOME/configs/konsole/GNOMETerminalLight.colorscheme" "$HOME/.local/share/konsole/GNOMETerminalLight.colorscheme"
+install_link "$HOME/configs/konsole/default.profile" "$HOME/.local/share/konsole/default.profile"
 
 # micro
 install_link ~/configs/micro/bindings.json ~/.config/micro/bindings.json
