d001907929b9aa6df06229f963bb9a1cdd3eec24
user <user@alpine-vm.my.domain>
2026-02-13 15:15:44 +0500

    fix setup.sh to work with sudo by using functions and CONFIGS_DIR

--- install_scripts/epm.sh
+++ install_scripts/epm.sh
@@ -7,59 +7,61 @@ case "$(uname -s)" in
       . /etc/os-release
       case "$ID" in
         rocky)
-          alias updateall="dnf clean all && dnf makecache && dnf upgrade -y"
-          alias install="dnf install -y"
-          alias uninstall="dnf remove -y"
+          updateall() { dnf clean all && dnf makecache && dnf upgrade -y; }
+          install() { dnf install -y "$@"; }
+          uninstall() { dnf remove -y "$@"; }
           ;;
         arch)
-          alias updateall='yay -Syu --devel --timeupdate; yay -Sc'
-          alias install="yay -S"
-          alias uninstall="yay -Rns"
-          alias updatemirrors="cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak; rate-mirrors arch | sudo tee /etc/pacman.d/mirrorlist; sudo pacman -Syy"
+          updateall() { yay -Syu --devel --timeupdate; yay -Sc; }
+          install() { yay -S "$@"; }
+          uninstall() { yay -Rns "$@"; }
+          updatemirrors() { cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak; rate-mirrors arch | sudo tee /etc/pacman.d/mirrorlist; sudo pacman -Syy; }
           ;;
         debian|ubuntu|droidian)
-          alias updateall='apt update && apt upgrade && apt dist-upgrade'
-          alias install="apt install"
-          alias uninstall="apt -y remove"
+          updateall() { apt update && apt upgrade && apt dist-upgrade; }
+          install() { apt install "$@"; }
+          uninstall() { apt -y remove "$@"; }
           ;;
         opensuse-tumbleweed|opensuse-leap)
           if [[ $UID != 0 && $EUID != 0 ]]; then
-            alias zypper='sudo zypper'
+            _zypper() { sudo zypper "$@"; }
+          else
+            _zypper() { zypper "$@"; }
           fi
-          alias updateall='zypper refresh && zypper dup'
-          alias install="zypper -n install"
-          alias uninstall="zypper -n remove"
+          updateall() { _zypper refresh && _zypper dup; }
+          install() { _zypper -n install "$@"; }
+          uninstall() { _zypper -n remove "$@"; }
           ;;
         alpine)
-          alias updateall='apk upgrade --available'
-          alias install='apk add'
-          alias uninstall='apk del'
+          updateall() { apk upgrade --available; }
+          install() { apk add "$@"; }
+          uninstall() { apk del "$@"; }
           ;;
         nixos)
-          alias updateall='echo "Use NixOS configuration to manage packages"'
+          updateall() { echo "Use NixOS configuration to manage packages"; }
           install() { echo "  $*"; }
-          alias uninstall='echo "Use NixOS configuration to manage packages"'
+          uninstall() { echo "Use NixOS configuration to manage packages"; }
           ;;
         *)
-          alias updateall='echo "Unknown Linux distribution"'
-          alias install='echo "Unknown Linux distribution"'
-          alias uninstall='echo "Unknown Linux distribution"'
+          updateall() { echo "Unknown Linux distribution"; }
+          install() { echo "Unknown Linux distribution"; }
+          uninstall() { echo "Unknown Linux distribution"; }
           ;;
       esac
     else
-      alias updateall='echo "Unknown Linux distribution"'
-      alias install='echo "Unknown Linux distribution"'
-      alias uninstall='echo "Unknown Linux distribution"'
+      updateall() { echo "Unknown Linux distribution"; }
+      install() { echo "Unknown Linux distribution"; }
+      uninstall() { echo "Unknown Linux distribution"; }
     fi
     ;;
   Darwin)
-    alias updateall='brew update; brew upgrade --no-quarantine --greedy; brew cleanup --prune=all'
-    alias install='brew install --no-quarantine'
-    alias uninstall='brew remove'
+    updateall() { brew update; brew upgrade --no-quarantine --greedy; brew cleanup --prune=all; }
+    install() { brew install --no-quarantine "$@"; }
+    uninstall() { brew remove "$@"; }
     ;;
   *)
-    alias updateall='echo "Unknown operating system"'
-    alias install='echo "Unknown operating system"'
-    alias uninstall='echo "Unknown operating system"'
+    updateall() { echo "Unknown operating system"; }
+    install() { echo "Unknown operating system"; }
+    uninstall() { echo "Unknown operating system"; }
     ;;
 esac
