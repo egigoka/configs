cec350838afdfd7bd0a2db065f42697970f44cd7
egigoka <egigoka@gmail.com>
2026-02-18 20:17:44 -0800

    fix string sub parsing negative timezone offset on macOS

--- fish/functions/get_monday_iso8601.fish
+++ fish/functions/get_monday_iso8601.fish
@@ -4,7 +4,7 @@ function get_monday_iso8601 --description "Get ISO 8601 timestamp for last Monda
     if test "$os" = "Darwin"
         set ts (date -v-mon -v0H -v0M '+%Y-%m-%dT%H:%M:%S')
         set tz (date +%z)  # e.g., "-0700"
-        set tz_formatted (string sub -l 3 $tz):(string sub -s 4 $tz)
+        set tz_formatted (string sub -l 3 -- $tz):(string sub -s 4 -- $tz)
         echo "$ts$tz_formatted"
     else
         set dow (date +%u)             # Day of week: 1..7, Mon=1
