4583b8da1891e7c0e685033b749af6f0b502d63d
egigoka <egigoka@gmail.com>
2026-04-27 12:02:12 +0500

    resolve merge conflict: updateall skip brew when root on macOS

--- fish/config.fish
+++ fish/config.fish
@@ -321,7 +321,11 @@ if status is-interactive
       end
     case Darwin
       # mAcos
-      abbr --add updateall --position command fisher update \&\& brew update \&\& brew upgrade --greedy \&\& brew cleanup --prune=all \&\& sudo git -C /opt/macports-wine pull \&\& sudo port selfupdate \&\& sudo ~/configs/install_scripts/autopatch_qbittorrent_macports.sh \&\& sudo port upgrade outdated \&\& sudo port reclaim --enable-reminders
+      if test (id -u) -eq 0
+        abbr --add updateall --position command fisher update \&\& sudo git -C /opt/macports-wine pull \&\& sudo port selfupdate \&\& sudo port upgrade outdated \&\& sudo port reclaim --enable-reminders \&\& ~/configs/install_scripts/autopatch_qbittorrent_macports.sh
+      else
+        abbr --add updateall --position command fisher update \&\& brew update \&\& brew upgrade --greedy \&\& brew cleanup --prune=all \&\& sudo git -C /opt/macports-wine pull \&\& sudo port selfupdate \&\& sudo port upgrade outdated \&\& sudo port reclaim --enable-reminders \&\& ~/configs/install_scripts/autopatch_qbittorrent_macports.sh
+      end
       abbr --add install   --position command brew install
       abbr --add uninstall --position command brew remove
     case '*'
