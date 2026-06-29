3109b6b53d51d011b3815d0a3faef40a046d948c
egigoka <egigoka@gmail.com>
2026-06-29 11:04:31 +0500

    home.nix: add extra-cmake-modules

--- nix/home.nix
+++ nix/home.nix
@@ -79,6 +79,7 @@ in
     gcc           
     clang-tools   # provides clang-format
     cmake         
+    kdePackages.extra-cmake-modules
     ninja         
     gnumake       
     nix-index     # provides `nix-locate` (find which pkg ships a file); `nix search` is built into nix
